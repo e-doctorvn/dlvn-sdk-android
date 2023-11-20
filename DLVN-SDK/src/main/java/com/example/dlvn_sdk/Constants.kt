9424b4577eca01cb0b647c0052b20997b006aa3d
Danh MC <maicongdanh1252@gmail.com>
2023-11-20 15:40:56 +0700

    update

--- DLVN-SDK/src/main/java/com/example/dlvn_sdk/Constants.kt
+++ DLVN-SDK/src/main/java/com/example/dlvn_sdk/Constants.kt
@@ -1,9 +1,9 @@
 package com.example.dlvn_sdk
 
 object Constants {
-//    - Thêm 3 nút ở view picture in picture webview
-//    - Pop up thông báo sắp hết thời gian gọi
-    const val healthConsultantUrlDev = "https://e-doctor.dev/tu-van-suc-khoe" //"http://192.168.120.250:3000/tu-van-suc-khoe"  //"https://khuat.dai-ichi-life.com.vn/tu-van-suc-khoe"
+//    - Thêm 3 nút ở view picture in picture webview [DONE]
+//    - Pop up thông báo sắp hết thời gian gọi [TO DO]
+    const val healthConsultantUrlDev = "https://e-doctor.dev/tu-van-suc-khoe/ho-so-gia-dinh" //"http://192.168.120.250:3000/tu-van-suc-khoe"  //"https://khuat.dai-ichi-life.com.vn/tu-van-suc-khoe"
     const val healthConsultantUrlProd = "https://kh.dai-ichi-life.com.vn/tu-van-suc-khoe"
     const val edrApiUrlDev = "https://virtual-clinic.api.e-doctor.dev/"
     const val edrApiUrlProd = "https://virtual-clinic.api.edoctor.io/"
