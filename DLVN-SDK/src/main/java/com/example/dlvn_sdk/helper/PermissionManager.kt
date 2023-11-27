df03dd620a978eb1499a131de19a32e4c5f0f091
Danh MC <maicongdanh1252@gmail.com>
2023-11-27 14:28:10 +0700

    Update PermissionManager.kt

--- DLVN-SDK/src/main/java/com/example/dlvn_sdk/helper/PermissionManager.kt
+++ DLVN-SDK/src/main/java/com/example/dlvn_sdk/helper/PermissionManager.kt
@@ -1,12 +1,15 @@
 package com.example.dlvn_sdk.helper
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.provider.Settings
 import androidx.activity.result.ActivityResultLauncher
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.dlvn_sdk.EdoctorDlvnSdk
@@ -14,6 +17,11 @@ import com.example.dlvn_sdk.R
 
 
 object PermissionManager {
+    private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
+    private const val MICROPHONE_PERMISSION_REQUEST_CODE = 1002
+    private const val NOTIFICATION_PERMISSION_REQUEST_CODE = 1003
+    const val ALL_PERMISSIONS_REQUEST_CODE = 1004
+
     fun handleRequestPermission(activity: Activity, permission: String, requestPermissionLauncher: ActivityResultLauncher<String>) {
         when {
             ContextCompat.checkSelfPermission(
@@ -50,5 +58,55 @@ object PermissionManager {
     fun getRatinaleDisplayStatus(context: Context, permission: String?): Boolean {
         val genPrefs = context.getSharedPreferences("GENERIC_PREFERENCES", Context.MODE_PRIVATE)
         return genPrefs.getBoolean(permission, false)
+    }
+
+    fun checkCameraPermission(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            context.checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        } else true
+    }
+
+    fun requestCameraPermission(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            activity.requestPermissions(
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    fun checkMicrophonePermission(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            context.checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+        } else true
+    }
+
+    fun requestMicrophonePermission(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            activity.requestPermissions(
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                MICROPHONE_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    fun checkNotificationPermission(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            context.checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+        } else true
+    }
+
+    fun requestNotificationPermission(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            activity.requestPermissions(
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                NOTIFICATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun checkBluetoothPermission(context: Context): Boolean {
+        return context.checkSelfPermission(Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED
     }
 }
